57deb1cd3166869da0c58ef42c60bf93ef26b0f0
KHawaja Abrar <83450580+KhAbrar@users.noreply.github.com>
2021-10-22 12:05:17 +0500

    Update q.cpp

--- q.cpp
+++ q.cpp
@@ -1,35 +1,17 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 int main()
-
 {
-
     int maxc, minc, step =10, Centimeters, Inches;
-
     printf("Please enter the max value in centimeters\n");
-
    scanf("%d", &maxc);
-
    printf("Please enter the min value in centimeters\n");
-
    scanf("%d", &minc);printf("   Centimeters       Inches\n ");
-
- 
-
- 
-
     for (Centimeters = maxc; Centimeters >= minc; Centimeters-= step)
 
     {
-
         /*do Math*/
-
         Inches = Centimeters * 0.3937;
-
             }
-
  return 0;
-
 }
